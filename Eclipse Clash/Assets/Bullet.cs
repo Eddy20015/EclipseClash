0c427653ca0241904b5195acf11449025c1ba129
bluesilver421 <bluesilver421@gmail.com>
2024-05-18 23:54:37 -0700

    enemy damage
    enemies now have health, will take damage when shot, will die once their health is depleted

--- Eclipse Clash/Assets/Bullet.cs
+++ Eclipse Clash/Assets/Bullet.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 20f; //speed of bullet 
+    public float speed = 20f; //speed of bullet
+    public int damage = 40;
     public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
 
         rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo) //when bullet hits something
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+
     }
 
 }
